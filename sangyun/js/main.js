5ec1df92a33811692b3539f8f80b9bd8b473fc03
sangyun lee <sororiri@gmail.com>
2022-10-07 00:59:44 +0900

    Add : (미션 8) 아이디 검색 기능 추가

--- sangyun/js/main.js
+++ sangyun/js/main.js
@@ -1,23 +1,69 @@
+let profiles = undefined;
+fetch('../data/profiles.json')
+  .then(response => response.json())
+  .then(obj => {
+    profiles = obj.profiles;
+  });
+
+
+
+
+// search 관련 리스너 추가
+const searchInput = document.querySelector(".search-input");
+searchInput.addEventListener('input', () => {
+  const commentResult = document.querySelector(".search-results");
+  commentResult.textContent = '';
+
+  if (!searchInput.value) {
+    return;
+  }
+
+  [...profiles]
+    .filter((profile) => (profile.username.includes(searchInput.value)))
+    .forEach((filterdProfile, index) => {
+      if (index > 4) {
+        return;
+      }
+      const {userImgUrl, username, comment} = filterdProfile;
+      const profileElement = htmlToElement(`
+              <div class="search-result">
+                <div class="result-profile-img-wrapper">
+                  <img class="result-profile-img" src="${userImgUrl}" alt="">
+                </div>
+                <div class="profile-right-wrapper">
+                  <div class="profile-username">${username}</div>
+                  <div class="profile-comment">${comment}</div>
+                </div>
+              </div>`);
+      commentResult.append(profileElement);
+    })
+
+})
+
+
 const feeds = document.querySelectorAll(".feed");
-
-
+// Feed 관련 리스너 추가
 feeds.forEach(feed => {
   const input = feed.querySelector(".input-comment");
-  ['keypress', 'click'].forEach((eventType) => {
+  ['input', 'click'].forEach((eventType) => {
     // 버튼이나 엔터로 댓글을 추가하는 리스너 추가
     feed.addEventListener(eventType, e => {
       console.log("event type: ", e.type);
       let {classList} = e.target;
       let {type} = e;
-      if (type == "click" && !classList.contains("add-comment")) {
+
+      // 예외처리
+      {
+        if (type == "click" && !classList.contains("add-comment")) {
+            return;
+        }
+        if (type == "input"
+          &&(e.key != 'Enter' || !classList.contains("input-comment"))) {
           return;
-      }
-      if (type == "keypress"
-        &&(e.key != 'Enter' || !classList.contains("input-comment"))) {
-        return;
-      }
-      if (!input.value) {
-        return;
+        }
+        if (!input.value) {
+          return;
+        }
       }
 
       writeComment.call(feed, input.value, "leeSang");
@@ -66,7 +112,6 @@ fetch('../data/comments.json')
   !! this가 feed라는 전제하에 동작함.
  */
 function writeComment(content, writer) {
-  console.dir(this);
   const comment_list = this.querySelector(".comment-list");
   let xmlString = `
   <div class="comment-text">
