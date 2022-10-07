c072ce9066088bdd8ebfc6b2ca55d656b0355c29
Jtree03 <wowns0903@gmail.com>
2022-10-07 16:44:53 +0900

    절대 경로 상대 경로로 변경

--- jojaejun/js/main.js
+++ jojaejun/js/main.js
@@ -1,5 +1,7 @@
 const commentList = document.getElementsByClassName("feed-comment-list");
-const commentButtonList = document.getElementsByClassName("feed-comment-input-button");
+const commentButtonList = document.getElementsByClassName(
+  "feed-comment-input-button"
+);
 
 const commentArray = Array.from(commentList);
 const commentButtonArray = Array.from(commentButtonList);
@@ -7,7 +9,8 @@ const commentButtonArray = Array.from(commentButtonList);
 commentButtonArray.forEach((button, index) => {
   button.addEventListener("click", () => {
     const input = document.getElementsByClassName("feed-comment-input")[index];
-    const commentList = document.getElementsByClassName("feed-comment-list")[index];
+    const commentList =
+      document.getElementsByClassName("feed-comment-list")[index];
 
     const commentCard = document.createElement("div");
     commentCard.classList.add("feed-comment-card");
@@ -23,3 +26,15 @@ commentButtonArray.forEach((button, index) => {
     commentList.append(commentCard);
   });
 });
+
+function getCommentList() {
+  fetch("./data/commentList.json")
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      json.forEach((comment) => {
+        const commentList = document.createElement("div");
+      });
+    });
+}
+getCommentList();
