57371dbf4266094a55e43b1e845b485d92ffc7c4
Jtree03 <wowns0903@gmail.com>
2022-10-07 17:06:43 +0900

    로그인 인풋 관련 updateLoginButtonDOM 함수 추출

--- jojaejun/js/login.js
+++ jojaejun/js/login.js
@@ -7,27 +7,22 @@ let pwValue = "";
 
 idInput.addEventListener("input", (e) => {
   idValue = e.target.value;
-  console.log(idValue, pwValue);
-  console.log(loginButton.disabled);
-  console.log(loginButton.style);
 
-  if (idValue !== "" && pwValue !== "") {
-    loginButton.disabled = false;
-    loginButton.style.cursor = "pointer";
-  } else {
-    loginButton.disabled = true;
-    loginButton.style.cursor = "not-allowed";
-  }
+  updateLoginButtonDOM();
 });
 
 pwInput.addEventListener("input", (e) => {
   pwValue = e.target.value;
 
-  if (idValue !== "" && pwValue !== "") {
+  updateLoginButtonDOM();
+});
+
+function updateLoginButtonDOM() {
+  if (idValue !== "" && idValue.includes("@") && pwValue !== "") {
     loginButton.disabled = false;
     loginButton.style.cursor = "pointer";
   } else {
     loginButton.disabled = true;
     loginButton.style.cursor = "not-allowed";
   }
-});
+}
