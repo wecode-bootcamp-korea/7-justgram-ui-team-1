c7cc0b7054d92ceaa549840deb2a43d4edaeaa8a
sangyun lee <sororiri@gmail.com>
2022-10-10 14:39:11 +0900

    Add : [Mission 3,4] JUSTGRAM Backend Project (Basic) - 게시글 목록 조회  및 수정하기

--- sangyun/backend/app.js
+++ sangyun/backend/app.js
@@ -62,6 +62,14 @@ http statusCode: ${res.statusCode}
 app.post('/post', (req, res) => {
   const {title, content, userId} = req.body;
   // 예외처리
+  const existUserId = users.find((user) => (user.id == userId));
+  if (!existUserId) {
+    res.status(400).send(
+      `there is no matched userId
+http statusCode: ${res.statusCode}
+    `);
+    return;
+  }
   if (!title || !content || !userId) {
     res.status(400).send(
       `plz send detail request for {title, content, userId}
@@ -84,8 +92,54 @@ http statusCode: ${res.statusCode}
   });
 });
 
+app.get('/post', (req, res) => {
+  const detailPosts = posts.map((post) => {
+    return {
+      postingId : post.id,
+      postingTitle : post.title,
+      postingContent : post.content,
+      userID : post.userId,
+      userName : users.find((user) => (user.id == post.userId)).name
+    }
+  })
+  res.send(detailPosts);
+})
 
+app.patch('/post/:id', (req, res) => {
+  const postId = req.params.id;
+  const foundedPost = posts.find((post) => (post.id == postId));
 
+  if (!foundedPost) {
+    res.status(400).send(
+      `there is no matched postId
+http statusCode: ${res.statusCode}
+    `);
+    return;
+  }
+
+  const {title, content} = req.body;
+
+  if (!title || !content) {
+    res.status(400).send(
+      `plz send detail request for {title, content}
+http statusCode: ${res.statusCode}
+    `);
+    return;
+  }
+
+  foundedPost.title = title;
+  foundedPost.content = content;
+
+  updatedPostDetail = {
+      postingId : foundedPost.id,
+      postingTitle : foundedPost.title,
+      postingContent : foundedPost.content,
+      userID : foundedPost.userId,
+      userName : users.find((user) => (user.id == foundedPost.userId)).name
+  };
+
+  res.send(updatedPostDetail);
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
